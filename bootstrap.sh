45d9475eb681093278a2fec70ffd7b8e573a3ee8
bguedes <guedes.bruno@gmail.com>
2022-11-26 13:00:01 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,4 @@ wget https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vag
 vagrant box add CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --name centos7
 
 git clone https://github.com/bguedes/edge2ai_vagrant.git
-cd edge2ai_vagrant
+cd ./edge2ai_vagrant
