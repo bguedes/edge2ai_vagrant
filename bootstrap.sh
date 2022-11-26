73aef3cf852305e55d1a07d951d5d9d976c4a96a
bguedes <guedes.bruno@gmail.com>
2022-11-26 13:48:31 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 VAGRANT_BOX_LIST=$(vagrant box list)
 
-if [[ ! $VAGRANT_BOX_LIST == *"centos7"* ]]; then
+if [[ $VAGRANT_BOX_LIST != *"centos7"* ]]; then
   wget https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --no-check-certificate
 fi
 
