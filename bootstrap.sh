062981b5becb0c162c2cbcd0b97ea2eccbd6f079
bguedes <guedes.bruno@gmail.com>
2022-11-26 12:07:48 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-vagrant plugin install vagrant-vbguest
-
 wget https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --no-check-certificate
 
 vagrant box add CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --name centos7
