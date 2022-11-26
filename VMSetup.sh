5758f913e8724f21c177d52aaa13e6fc6fe1aa7e
bguedes <guedes.bruno@gmail.com>
2022-11-26 12:12:51 +0100

    Update VMSetup.sh

--- VMSetup.sh
+++ VMSetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "> Installing required tools"
 if  [ -n "$(command -v yum)" ]; then
     echo ">> Detected yum-based Linux"
@@ -9,10 +10,17 @@ if  [ -n "$(command -v yum)" ]; then
     sudo yum install -y yum-utils
     echo "> Installing docker"
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-    sudo systemctl start docker
+    sudo yum -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    
+    sudo su
+    systemctl start docker
+    systemctl enable docker
+    systemctl restart docker
+    #sudo systemctl start docker
 fix
 
 cd ~
-git clone https://github.com/bguedes/edge2ai_vagrant.git
-cd cdp_pvc_onenode_demo
+git clone https://github.com/asdaraujo/edge2ai-workshop.git
+cd edge2ai-workshop.git
+
+cp edge2ai-workshop/setup/terraform/resources/stack.stack.cdp717p.sh edge2ai-workshop/setup/terraform/resources/stack.sh
