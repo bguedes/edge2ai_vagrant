b615db4a8a74de9906408b8ad53157063817252f
bguedes <guedes.bruno@gmail.com>
2022-11-30 22:54:13 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,7 @@ if [[ $VAGRANT_BOX_LIST != *"centos7"* ]]; then
   wget https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --no-check-certificate
 fi
 
-
-https://raw.githubusercontent.com/bguedes/edge2ai_vagrant/main/Vagrantfile
-https://raw.githubusercontent.com/bguedes/edge2ai_vagrant/main/VMSetup.sh
+wget https://raw.githubusercontent.com/bguedes/edge2ai_vagrant/main/Vagrantfile --no-check-certificate
+wget https://raw.githubusercontent.com/bguedes/edge2ai_vagrant/main/VMSetup.sh --no-check-certificate
 
 chmod +x VMSetup.sh
