45bff9ae24249345979afb61bd255548ac057662
bguedes <guedes.bruno@gmail.com>
2022-11-30 22:56:35 +0100

    Update windows_bootstrap.sh

--- windows_bootstrap.sh
+++ windows_bootstrap.sh
@@ -4,6 +4,7 @@ VAGRANT_BOX_LIST=$(vagrant box list)
 
 if [[ $VAGRANT_BOX_LIST != *"centos7"* ]]; then
     Invoke-WebRequest -Uri https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box -UseBasicParsing -OutFile CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box
+    vagrant box add CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --name centos7
 fi
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/bguedes/edge2ai_vagrant/main/VMSetup.sh -UseBasicParsing -OutFile VMSetup.sh
